846dee89fe984afc73aeece854ac9e8c6a4071fd
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-10-15 15:38:23 +0000

    Practice Control Flow like if/else, switch, loop

---
+++ Basic/control-flow/genreMessage.js
@@ -0,0 +1,34 @@
+let genre = "Action";
+
+switch(genre) {
+    case "Action":
+        console.log("You love fight scenes!");
+        break;
+
+    case "Comedy":
+        console.log("You love humour!");
+        break;
+    
+    case "Drama":
+        console.log("You love Romantic!")
+        break;
+
+    case "Horror":
+        console.log("You love scary things!");
+        break;
+
+    case "Adventure":
+        console.log("You love challenges!");
+        break;
+
+    case "Fantasy":
+        console.log("You love supernatural!");
+        break;
+
+    case "Mystery":
+        console.log("You love stranger things!");
+        break;
+
+    default:
+        console.log("Unknown genre!");
+}
---
+++ Basic/control-flow/movieRatingChecker.js
@@ -0,0 +1,11 @@
+let score = 4;
+
+if(score >= 9){
+    console.log("Excellent");
+}else if(score >= 7){
+    console.log("Good");
+}else if(score >= 5){
+    console.log("Average");
+}else{
+    console.log("Poor");
+}
