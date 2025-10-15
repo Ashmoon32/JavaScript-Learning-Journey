e3a062a603953af075c92f34f6047e0a655915b8
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-10-15 15:37:25 +0000

    Practice Variable and Data Types

---
+++ Basic/variable/script.js
@@ -0,0 +1,61 @@
+// let name = "Movie App";
+// let rating = 4.8;
+// let isReleased = true;
+// let description;
+// let director = null;
+// let movie = {title: "Inception", year: 2010 };
+// let tags = ["Sci-fi","Action","Mind-blown"];
+
+// console.log(typeof name);
+// console.log(typeof rating);
+// console.log(typeof movie);
+// console.log(typeof isReleased);
+// console.log(typeof director);
+// console.log(typeof tags);
+// console.log(typeof director);
+
+
+// let friendName = "Win Kyi";
+// let age = 21;
+// let isSingle = false;
+// let haveChildrens = null;
+// let wife;
+
+// console.log(friendName);
+// console.log(typeof friendName);
+// console.log(age);
+// console.log(typeof age);
+// console.log(isSingle);
+// console.log(typeof isSingle);
+// console.log(haveChildrens);
+// console.log(typeof haveChildrens);
+// console.log(wife);
+// console.log(typeof wife);
+
+
+
+
+let x = 10;
+let y = 5;
+let larger = ( x > y )? "x" : "y";
+let sum = x + y;
+console.log("Sum: " + sum);
+if(x>y){
+    console.log("Larger: " + larger);
+}else{
+    console.log("Larger: " + larger);
+}
+
+let isPositive = true;
+let isNegative = false;
+if( x > 0 && y > 0) {
+    console.log("Both positive: " + isPositive);
+// }else if(x > 0 && y < 0){
+//     console.log("Both positive: " + isNegative);
+// }else if(x < 0 && y > 0){
+//     console.log("Both positive: " + isNegative);
+// }else if(x < 0 && y < 0){
+//     console.log("Both positive: " + isNegative);
+}else{
+    console.log("Both positive: " + isNegative);
+}
