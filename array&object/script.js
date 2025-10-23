0ac68c152491cc2854db34d6ff13b6e5f742df76
Ashmoon32 <nyeinmaung3021@gmail.com>
2025-10-23 07:14:32 +0000

    Practice Array

---
+++ array&object/script.js
@@ -0,0 +1,17 @@
+// Two ways to creat an Array
+
+// First way
+let student1 = new Array("John Doe", 21, 11);
+
+console.log("First Student : ");
+console.log("Name : " + student1[0]);
+console.log("Age : " + student1[1]);
+console.log("Grade : " + student1[2]);
+
+console.log("--------------------------------- ");
+// Second way
+let student2 = ["Albert", 22, 12];
+console.log("Second Student :");
+console.log(`Name : ${student2[0]}`);
+console.log(`Age : ${student2[1]}`);
+console.log(`Grade : ${student2[2]}`);
